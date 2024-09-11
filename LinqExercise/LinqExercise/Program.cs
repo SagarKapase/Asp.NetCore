31ca94d00ce819219078eaf4489059e9db01099d
SagarKapase <sagarkapase139@gmail.com>
2024-09-11 14:58:05 +0530

    initial commit

---
+++ LinqExercise/LinqExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LinqExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Sagar", "John", "Thomas", "Shelby", "Peaky Blinder" };
+
+            //Linq query
+            var myLinqQuery = from name in names
+                              where name.Contains('a')
+                              select name;
+
+            //query execution
+            foreach (var name in myLinqQuery)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
