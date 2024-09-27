2a49eda898fa55022c4191b6d4bf0f77f3ed79ff
SagarKapase <sagarkapase139@gmail.com>
2024-09-27 14:48:30 +0530

    initial commit

---
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        DbSet<Category> Categories { get; set; }
+    }
+}
